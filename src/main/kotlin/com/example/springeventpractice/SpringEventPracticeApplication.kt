7dcbf4826683be47be67cdcb39394bd503ab2b72
Timo <developer.timo@gmail.com>
2023-10-22 21:27:26 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/springeventpractice/SpringEventPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springeventpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringEventPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringEventPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springeventpractice/SpringEventPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springeventpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringEventPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
