132102bd7b964cb2b6aea0c9d26b078b7e646017
Timo <developer.timo@gmail.com>
2023-10-22 21:52:00 +0900

    feat: 이벤트 발행, 수신 비동기 - 스레드 확인 테스트

--- src/main/kotlin/com/example/springeventpractice/reciever/SmsEventHandler.kt
+++ src/main/kotlin/com/example/springeventpractice/reciever/SmsEventHandler.kt
@@ -1,24 +1,32 @@
 package com.example.springeventpractice.reciever
 
 import com.example.springeventpractice.event.RegisteredEvent
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
+import org.springframework.core.annotation.Order
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
 class SmsEventHandler {
 
+    @Order(1)
     @Async
     @EventListener
     fun sendSms(event: RegisteredEvent) {
+        logger.info("sendSms")
         println("${event.name}님에게 가입 축하 메시지를 전송했습니다.")
     }
 
     @Async
     @EventListener
     fun makeCoupon(event: RegisteredEvent) {
-        Thread.sleep(500)
-        throw RuntimeException("쿠폰 발행 에러 발생")
+        logger.info("makeCoupon")
         println("${event.name}님에게 쿠폰을 전송했습니다.")
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(this::class.java)
     }
 }
