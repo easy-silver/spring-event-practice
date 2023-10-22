3a9e75824dcbd26d4d90d4a10a4e0d01838573a4
Timo <developer.timo@gmail.com>
2023-10-22 21:41:31 +0900

    feat: 이벤트 발행, 수신 동기 처리 확인

--- src/main/kotlin/com/example/springeventpractice/reciever/SmsEventHandler.kt
+++ src/main/kotlin/com/example/springeventpractice/reciever/SmsEventHandler.kt
@@ -9,11 +9,13 @@ class SmsEventHandler {
 
     @EventListener
     fun sendSms(event: RegisteredEvent) {
+        Thread.sleep(2000)
         println("${event.name}님에게 가입 축하 메시지를 전송했습니다.")
     }
 
     @EventListener
     fun makeCoupon(event: RegisteredEvent) {
+        Thread.sleep(1000)
         println("${event.name}님에게 쿠폰을 전송했습니다.")
     }
 }
