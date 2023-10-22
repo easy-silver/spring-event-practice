c635edd01072cc042d6ee085dc193faaa0bd2967
Timo <developer.timo@gmail.com>
2023-10-22 21:46:47 +0900

    feat: 이벤트 발행, 수신 비동기 에러 발생 테스트

--- src/main/kotlin/com/example/springeventpractice/reciever/SmsEventHandler.kt
+++ src/main/kotlin/com/example/springeventpractice/reciever/SmsEventHandler.kt
@@ -11,14 +11,14 @@ class SmsEventHandler {
     @Async
     @EventListener
     fun sendSms(event: RegisteredEvent) {
-        Thread.sleep(2000)
         println("${event.name}님에게 가입 축하 메시지를 전송했습니다.")
     }
 
     @Async
     @EventListener
     fun makeCoupon(event: RegisteredEvent) {
-        Thread.sleep(1000)
+        Thread.sleep(500)
+        throw RuntimeException("쿠폰 발행 에러 발생")
         println("${event.name}님에게 쿠폰을 전송했습니다.")
     }
 }
